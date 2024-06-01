ebc51b358262eb62dbb310a551f3d6a0bcc12eb8
Cesar Lopez Orihuela <clopez@EYSEN.MX>
2024-05-31 18:01:27 -0600

    Menu

---
+++ menu/README.md
@@ -0,0 +1,19 @@
+# Calculadora.
+
+Ejercicio de YouTube.
+
+Codigo 369
+
+## Lenguaje.
+
+| HTML || JavaScript || CSS |
+
+## Uso de:
+
+> Funciones
+
+> Condicionales
+
+> Relacionales
+
+## Resultados:
---
+++ menu/script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector(".menuToggle");
+const navigation = document.querySelector(".navigation");
+
+menuToggle.oneclick =function(){
+    navigation.classList.toggle('open')
+}
